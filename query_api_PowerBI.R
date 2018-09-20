8dfcb194bf20af26cb15fc8dec2abac9c0c0447d
Jonas Lauer <jonaslauer@jonass-mbp-2.home>
2018-09-20 16:43:01 +0200

    Update to version 2

--- query_api_PowerBI.R
+++ query_api_PowerBI.R
@@ -3,40 +3,57 @@
 library(httr)
 library(data.table)
 library(lubridate)
+library(ggplot2)
 
-#def variable
-startdate_query = strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz = "UTC")
-enddate_query = strftime(enddate,format='%Y-%m-%dT%H:%M:%OSZ', tz = "UTC")
-model = {}
-ensSelect = {}
-
-#URL
-query = if(request_type == "timeseries")
+query_user_features = function(username, password)
 {
-  sprintf("https://%s:%s@api.meteomatics.com/%s--%s:%s/%s/%s/csv", username, password, startdate_query, enddate_query, interval, parameters, coordinate)
-} else {
-  sprintf("https://%s:%s@api.meteomatics.com/%s/%s/%s/csv", username, password, startdate_query, parameters, coordinate)
+  r=GET(sprintf('https://%s:%s@api.meteomatics.com/user_stats_json', username, password))
+  j=jsonlite::fromJSON(content(r, 'text'))
+  res <- logical(3)
+  names(res) <- c('area request option', 'historic request option', 'model select option')
+  res['area request option'] = j$`user statistics`$`area request option`
+  res['historic request option'] = j$`user statistics`$`historic request option`
+  res['model select option'] = j$`user statistics`$`model select option`
+  return(res)
 }
 
-#Data for PowerBI
-if (request_type == "timeseries"){
-#Timeseries
-  #Data from the API
-  api_timeseries = function(path)
+api_timeseries = function(query)
+{
+  resp1 = GET(query)
+  con = textConnection(content(resp1,"text"))
+  parsed1 = read.csv(con, sep = ";")
+  structure(
+    list(content = parsed1)
+  )
+}
+  
+api_domain = function(query)
+{
+  resp1 = fread(query, skip=2, fill = TRUE)
+  structure(
+    list(content = resp1)
+  )
+}
+
+query_api = function(username, password, startdate, enddate, 
+                     interval, parameters, coordinate, 
+                     request_type="timeseries",
+                     model="mix"
+)
+{
+  #def Variabeln
+  startdate_query = strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
+  enddate_query = strftime(enddate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
+  ensSelect = {}
+  #URL
+  if(request_type == "timeseries")
   {
-    resp1 = GET(query)
-    con = textConnection(content(resp1,"text"))
-    parsed1 = read.csv(con, sep = ";")
-    structure(
-      list(content = parsed1)
+    query = sprintf("https://%s:%s@api.meteomatics.com/%s--%s:%s/%s/%s/csv?model=%s", 
+                    username, password, startdate_query, enddate_query, interval, parameters, coordinate, model
     )
-  }
   
-  result = api_timeseries("/(startdate_query)---(enddate_query):(interval)/(parameters)/(coordinate)/csv?")
-  
-  #Data
-  query_api = function(username, password, startdate, enddate, interval, parameters, coordinate)
-  {
+    result = api_timeseries(query)
+    #Data in new Dataframe
     df = as.data.frame(result$content)
     #Numbers of col and row
     r = nrow(df)
@@ -44,7 +61,7 @@ if (request_type == "timeseries"){
     #Number of Parameters
     n_r = match("validdate", names(df))+1
     n_p = ncol(df[n_r:c])
-    #Date
+    #Dates in right shape
     df$validdate = as.POSIXct(df$validdate,format='%Y-%m-%dT%H:%M:%OSZ', tz = 'UTC')
     df$validdate = with_tz(df$validdate, tzone = time_zone)
     #Right shape parameters
@@ -59,36 +76,31 @@ if (request_type == "timeseries"){
     df_timeseries['Parameter']= parameter$ind
     df_timeseries['Value']= parameter$values
     return(df_timeseries)
-  }
-  
-}else{
-#Domain (Rectangle)
-  #Data from the API
-  api_domain = function(path)
-  {
-    resp1 = fread(query)
-    structure(
-      list(content = resp1)
+
+  } else {
+    query = sprintf("https://%s:%s@api.meteomatics.com/%s/%s/%s/csv?model=%s",
+                    username, password, startdate_query, parameters, coordinate, model
     )
-  }
-  
-  result = api_domain("/(startdate_query)/(parameters)/(coordinate)/csv?")
-  
-  #Data
-  query_api = function(username, password, startdate, enddate, interval, parameters, coordinate)
-  {
+    
+    result = api_domain(query)
+    
+    #New Dataframe
     df = as.data.frame(result$content)
-    #Numbers of col and row
+    #Numbers of columns and rows
     r = nrow(df)
     c = ncol(df)
-    #Lon
+    #Longitude
     Lon = rep(c(t(df[1,2:c])), r-1)
     df_domain = data.frame(Lon)
-    #Lat
-    Lat = rep(df[2:r,1], each = c-1)
+    #Latitude
+    Lat = rep(df[1:r,1], each = c-1)
     df_domain["Lat"] = data.frame(as.numeric(Lat))
     #Values
-    df_domain["Values"] = data.frame(Values = c(t(df[2:c,2:r])))
+    df_domain["Values"] = data.frame(Values = c(t(df[1:r,2:c])))
+    mm = matrix(unlist(df_domain["Values"]), ncol = c -1 , byrow = TRUE)
+    image(z=t(mm[nrow(mm):1,]))
     return(df_domain)
-  }
+    
+    } 
+  
 }
